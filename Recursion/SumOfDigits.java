e77724f759dc146b1f38db0c3b15b98a621cb4bb
Srushti Jamghare <srushtij707@gmail.com>
2026-04-23 12:06:29 +0530

    Solved Sum of digits using recursion

---
+++ Recursion/SumOfDigits.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+         int n=123;
+         System.out.println(sum(n));
+    }
+    static int sum(int n)
+    {
+        if(n<10) return n;
+        return n%10+ sum(n/10);
+    }
+}
