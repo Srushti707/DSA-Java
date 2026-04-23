872ef5753746c28b317b1edf0f311d44e0260a37
Srushti Jamghare <srushtij707@gmail.com>
2026-04-23 16:33:20 +0530

    more functional code

--- Recursion/ReverseNum.java
+++ Recursion/ReverseNum.java
@@ -3,17 +3,19 @@ package Recursion;
 public class ReverseNum {
     public static void main(String[] args) {
         int n=121;
-        rev(n);
-        System.out.println(sum);
-        if(n==sum) System.out.println("palindrome");
+        int rev= rev(n, 0);
+        System.out.println(rev(n,0));
+
+        if(n==rev) System.out.println("palindrome");
         else System.out.println("Not palindrome");
     }
-    static int sum=0;
-    static int rev(int n)
+    static int rev(int n, int sum)
     {
-        if(n==0) return 0;
-        int num=n%10;
-        sum=sum*10 +num;
-        return rev(n/10) ;
+        if (n == 0) return sum;
+
+        int rem = n % 10;
+        sum = sum * 10 + rem;
+
+        return rev(n / 10, sum);
     }
 }
